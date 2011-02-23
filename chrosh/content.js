7fd5c66e1785f954efbab5bdeb74df7bf5358671
Christopher Finke <cfinke@gmail.com>
2011-02-23 05:09:49 +0000

    cd, ls, and history commands

---
+++ chrosh/content.js
@@ -0,0 +1,29 @@
+chrome.extension.onRequest.addListener(
+	function (request, sender, sendResponse) {
+		if (request.greeting == "ls") {
+			var rv_object = {};
+			
+			var links = document.getElementsByTagName("a");
+			
+			for (var i = 0, _len = links.length; i < _len; i++) {
+				if (links[i].hasAttribute("href")) {
+					var href = links[i].href;
+					var title = links[i].innerHTML.replace(/<[^>]+>/g, "").replace(/\s+/g, " ");
+					
+					rv_object[href] = title;
+				}
+			}
+			
+			var rv_array = [];
+			
+			for (var url in rv_object) {
+				rv_array.push({ "href" : url, "title" : rv_object[url] });
+			}
+			
+			sendResponse({ "status": true, "links": rv_array });
+		}
+		else {
+			sendResponse({});
+		}
+	}
+);
